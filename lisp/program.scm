2cbe045b9d564e147f28782f71e0b98f9a208c03
kevin <kevin.lin.p@gmail.com>
2010-04-20 12:17:10 -0400

    delete_element_at done

--- lisp/program.scm
+++ lisp/program.scm
@@ -1,32 +1,12 @@
-(define (last list)
-  (if (null? (cdr list))
-      (car list)
-      (last (cdr list)))
+(define (delete_element_at set counter)
+  (cond
+    [(zero? counter) (cdr set)]
+    [else (cons (car set) (delete_element_at (cdr set) (- counter 1)))])
   )
 
-(define (expand_single set previous)
-  (if (null? previous)
-      (expand_single )
-      ())
+(define (contain? )
   )
 
-
-(define (next_iter size list previous)
-  (if (null? previous) 
-      (next_iter (cdr previous) (expand_single (car previous) '()))
-      ())
-  )
-
-(define (initial_array size prev_array)
-  (if (positive? size)
-      (initial_array (- size 1) (cons (cons size '()) prev_array))
-      (list (list empty) prev_array))
-  )
-
-(define (build size sorted_array)
-  (next_iter size (last sorted_array) '())
-  )
-
-(begin
-  (build 4 (initial_array 4 '()))
+(begin 
+  (contain? '((1 2) (1 3) (1 4)) '(1 2))
   )
