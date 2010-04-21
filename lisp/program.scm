3ab98e7b085f96a5df91d9c82305a5e7551436e5
kevin <kevin.lin.p@gmail.com>
2010-04-21 01:29:30 -0400

    prepend_no_dup almost done

--- lisp/program.scm
+++ lisp/program.scm
@@ -1,3 +1,9 @@
+(define (last list)
+  (cond
+    [(null? (cdr list)) (car list)]
+    [else (last (cdr list))])
+  )
+
 (define (delete_element_at list pos)
   (cond
     [(zero? pos) (cdr list)]
@@ -13,11 +19,11 @@
      )
   )
 
-(define (subset? seqs_to_search seq_to_find)
+(define (subset? seq_to_find seqs_to_search)
   (cond
     [(null? seqs_to_search) #f]
     [(equal_list? (car seqs_to_search) seq_to_find) #t]
-    [else (subset? (cdr seqs_to_search) seq_to_find)])
+    [else (subset? seq_to_find (cdr seqs_to_search))])
   )
 
 (define (generate_subseqs seq pos)
@@ -30,6 +36,13 @@
   (generate_subseqs seq (- (length seq) 1))
   )
 
+(define (prepend_no_dup to_prepend original)
+  (cond
+    [(null? to_prepend) '()]
+    [(subset? (last to_prepend) original) (prepend_no_dup (delete_element_at to_prepend (- (length to_prepend) 1)) original)]
+    [else (cons (cons (prepend_no_dup (delete_element_at to_prepend (- (length to_prepend) 1)) original) (last to_prepend)) original)])
+  )
+
 (begin 
-  (subseqs '(1 2 3 4 5 6))
+  (prepend_no_dup '((1 2) (1 3)) '((1 3) (1 4)))
   )
