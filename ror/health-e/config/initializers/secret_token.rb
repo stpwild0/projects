0bd9b92a457a289ee8b42dda50cf2186314c03b8
Kevin Lin <kevin.lin.p@gmail.com>
2011-02-24 08:20:11 -0500

    health-e initialization

---
+++ ror/health-e/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthE::Application.config.secret_token = 'bf7a96905abd1aad16ceffda949a0a61cdb4bdc68059fb0e4d55141ff6b85a5e064b65e12b701ce9149cfe6a49c4ef9cda8e02696dfabd7c95f100e08e1433d3'
